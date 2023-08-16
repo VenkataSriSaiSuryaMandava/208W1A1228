20e2ab8c893f05a25c78f8ff572c250b656a5c38
VenkataSriSaiSuryaMandava <ssurya.mandava@gmail.com>
2023-08-16 20:51:00 +0530

    first commit

--- First Question/numbermanagement/numbers.js
+++ First Question/numbermanagement/numbers.js
@@ -2,17 +2,19 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 8008; // Use environment variable or default to 8008
 const TIMEOUT = 1000; // Timeout in milliseconds
 
 app.get('/numbers', async (req, res) => {
   try {
     const urlQueries = req.query.url;
+
     if (!urlQueries) {
-      return res.status(400).json({ error: 'Missing parameter url' });
+      return res.status(400).json({ error: 'Missing url parameter' });
     }
 
     const urls = Array.isArray(urlQueries) ? urlQueries : [urlQueries];
+    
     const fetchPromises = urls.map(async (url) => {
       try {
         const response = await axios.get(url, { timeout: TIMEOUT });
@@ -24,6 +26,8 @@ app.get('/numbers', async (req, res) => {
     });
 
     const results = await Promise.all(fetchPromises);
+    
+    // Merge and sort the numbers from all fetched data
     const mergedNumbers = results.flat();
     const uniqueNumbers = Array.from(new Set(mergedNumbers)).sort((a, b) => a - b);
 
